1f86a441d43f6ed1400fbeddb1942663064b1f45
tynegr <122130536+tynegr@users.noreply.github.com>
2024-04-21 13:59:02 +0300

    Update redis_task.py

--- homework/tasks/redis_task.py
+++ homework/tasks/redis_task.py
@@ -12,16 +12,9 @@ class UsersByTitleStorage:
         await self._client.aclose()
 
     async def save_item(self, user_id: int, title: str) -> None:
-        """
-        Напишите код для сохранения записей таким образом, чтобы в дальнейшем
-        можно было за один запрос получить список уникальных пользователей,
-        имеющих объявления с заданным заголовком.
-        """
-        # YOUR CODE GOES HERE
+
+        await self._client.sadd(title, user_id)
 
     async def find_users_by_title(self, title: str) -> list[int]:
-        """
-        Напишите код для поиска уникальных user_id, имеющих хотя бы одно объявление
-        с заданным title.
-        """
-        # YOUR CODE GOES HERE
+
+        return [int(user_id) for user_id in await self._client.smembers(title)]
